69694523e591c8d7e106a8d5c619dcc7ca8aaf33
complexitycollapse <complexitycollapse@protonmail.com>
2025-01-01 19:56:00 +0000

    Added React

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   test: {
     global: true
   },
-  plugins: [],
+  plugins: [react()],
   build: {
     outDir: './dist'
   }
